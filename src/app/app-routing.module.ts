bbd367421b0b9aaa70531e9d6d15dd2b33411621
Agustinaa01 <aserra2@mail.austral.edu.ar>
2022-09-06 13:38:50 -0300

    commit message

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'register',
+    loadChildren: () => import('./public/pages/register/register.module').then(m => m.RegisterModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./public/pages/login/login.module').then(m => m.LoginModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
