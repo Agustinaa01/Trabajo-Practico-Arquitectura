63c688b1c9547d316759facc37590d60a9a9d960
agusnexx <agusescoda@gmail.com>
2022-10-11 19:53:49 -0300

    Componente contactos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactsComponent } from './public/pages/contacts/contacts.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    ContactsComponent,
   ],
   imports: [
     BrowserModule,
